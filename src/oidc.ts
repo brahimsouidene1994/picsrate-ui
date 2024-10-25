df0a269af171c169cd261e45b3fdc2597a1f01a9
brahim dev <barhoumsouidene@gmail.com>
2024-10-25 01:48:30 +0100

    fix redirect

--- src/oidc.ts
+++ src/oidc.ts
@@ -1,8 +1,8 @@
 const oidcConfig = {
   authority: `${process.env.REACT_APP_KEYCLOAK_API}/realms/${process.env.REACT_APP_KEYCLOAK_REALM}`,
   client_id: `${process.env.REACT_APP_KEYCLOAK_CLIENT}`,
-  redirect_uri: window.location.origin, // Where Keycloak will redirect after login
-  post_logout_redirect_uri: window.location.origin, // Where to go after logout
+  redirect_uri: `${process.env.REACT_APP_UI}`, // Where Keycloak will redirect after login
+  post_logout_redirect_uri: `${process.env.REACT_APP_UI}`, // Where to go after logout
   scope: 'offline_access profile email', // Add any additional scopes here
   response_type: 'code', // Use the authorization code flow
 };
